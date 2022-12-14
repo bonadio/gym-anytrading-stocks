d62b4888d8459d09dd77093f8006657ebdf3d92a
bonadio <cesar.bonadio@gmail.com>
2022-12-14 02:16:02 +0000

    Simple env

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 
-# gym-anytrading2
+# gym-anytrading-stocks
 
-`AnyTrading2` is a fork of AnyTrading that includes Action "Do nothing" 
+`AnyTradingStocks` is a fork of AnyTrading that includes Action "Do nothing", only for stocks
 
 
 ## Installation
 
 ### From Repository
 ```bash
-git clone https://github.com/bonadio/gym-anytrading2
-cd gym-anytrading2
+git clone https://github.com/bonadio/gym-anytrading-stocks
+cd gym-anytrading-stocks
 pip install -e .
 
 ```
---
+++ gym_anytrading2/envs/trading_env.py
@@ -0,0 +1,270 @@
+import gym
+from gym import spaces
+from gym.utils import seeding
+import numpy as np
+from enum import Enum
+import matplotlib.pyplot as plt
+
+
+class Actions(Enum):
+    Sell = 0
+    Buy = 1
+    Do_nothing = 2
+
+
+
+class TradingEnv(gym.Env):
+
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, df, window_size):
+        assert df.ndim == 2
+
+        self.seed()
+        self.df = df
+        self.window_size = window_size
+        self.prices, self.signal_features = self._process_data()
+        self.shape = (window_size, self.signal_features.shape[1])
+
+        # spaces
+        self.action_space = spaces.Discrete(len(Actions))
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=self.shape, dtype=np.float64)
+
+        # episode
+        self._start_tick = self.window_size
+        self._end_tick = len(self.prices) - 1
+        self._done = None
+        self._current_tick = None
+        self._last_trade_tick = None
+        self._position = None
+        self._position_history = None
+        self._total_reward = None
+        self._total_profit = None
+        self._first_rendering = None
+        self.history = None
+
+        # fees
+        self.trade_fee_bid_percent = 0.0005  # unit
+        self.trade_fee_ask_percent = 0.0005  # unit
+
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+
+    def reset(self):
+        self._done = False
+        self._current_tick = self._start_tick
+        self._last_trade_tick = self._current_tick - 1
+        self._position = 0
+        self._position_history = (self.window_size * [None]) + [self._position]
+        self._total_reward = 0.
+        self._total_profit = 1.  # unit
+        self._first_rendering = True
+        self.history = {}
+        return self._get_observation()
+
+
+    def _calculate_reward(self, action):
+        step_reward = 0
+
+        current_price = self.prices[self._current_tick]
+        last_price = self.prices[self._current_tick - 1]
+        price_diff = current_price - last_price
+
+        # BUY
+        if action == Actions.Buy.value and self._position == 0:
+            self._position = 1
+            step_reward += price_diff
+            self._last_trade_tick = self._current_tick
+
+        elif action == Actions.Buy.value and self._position > 0:
+            step_reward += 0
+        elif action == Actions.Buy.value and self._position < 0:
+            self._position = 0
+            step_reward += -1 * price_diff
+            self._last_trade_tick = self._current_tick
+
+            # self._total_profit += step_reward
+
+        # SELL
+        elif action == Actions.Sell.value and self._position == 0:
+            self._position = -1
+            step_reward += -1 * price_diff
+            self._last_trade_tick = self._current_tick
+        elif action == Actions.Sell.value and self._position > 0:
+            self._position = 0
+            step_reward += price_diff
+            self._last_trade_tick = self._current_tick
+
+            # self._total_profit += step_reward
+        elif action == Actions.Sell.value and self._position < 0:
+            step_reward += 0
+
+        # DO NOTHING
+        elif action == Actions.Do_nothing.value and self._position > 0:
+            step_reward += price_diff
+        elif action == Actions.Do_nothing.value and self._position < 0:
+            step_reward += -1 * price_diff
+        elif action == Actions.Do_nothing.value and self._position == 0:
+            step_reward += -1 * abs(price_diff)
+
+
+        return step_reward
+
+
+    def step(self, action):
+        self._done = False
+        self._current_tick += 1
+
+        if self._current_tick == self._end_tick:
+            self._done = True
+
+        step_reward = self._calculate_reward(action)
+        self._total_reward += step_reward
+
+        self._position_history.append(self._position)
+        observation = self._get_observation()
+        info = dict(
+            total_reward = self._total_reward,
+            total_profit = self._total_profit,
+            position = self._position
+        )
+        self._update_history(info)
+
+        return observation, step_reward, self._done, info
+
+
+    def _get_observation(self):
+        return self.signal_features[(self._current_tick-self.window_size+1):self._current_tick+1]
+
+
+    def _update_history(self, info):
+        if not self.history:
+            self.history = {key: [] for key in info.keys()}
+
+        for key, value in info.items():
+            self.history[key].append(value)
+
+
+    def render(self, mode='human'):
+
+        def _plot_position(position, tick):
+            color = None
+            if position < 0:
+                color = 'red'
+            elif position > 0:
+                color = 'green'
+            elif position == 0:
+                color = 'yellow'
+            if color:
+                plt.scatter(tick, self.prices[tick], color=color)
+
+        if self._first_rendering:
+            self._first_rendering = False
+            plt.cla()
+            plt.plot(self.prices)
+            start_position = self._position_history[self._start_tick]
+            _plot_position(start_position, self._start_tick)
+
+        _plot_position(self._position, self._current_tick)
+
+        plt.suptitle(
+            "Total Reward: %.6f" % self._total_reward + ' ~ ' +
+            "Total Profit: %.6f" % self._total_profit
+        )
+
+        plt.pause(0.01)
+
+
+    def render_all(self, mode='human'):
+        window_ticks = np.arange(len(self._position_history))
+        plt.plot(self.prices)
+
+        short_ticks = []
+        long_ticks = []
+        do_nothing_ticks = []
+        for i, tick in enumerate(window_ticks):
+            if self._position_history[i] < 0:
+                short_ticks.append(tick)
+            elif self._position_history[i] > 0 :
+                long_ticks.append(tick)
+            elif self._position_history[i] == 0 :
+                do_nothing_ticks.append(tick)
+
+        plt.plot(short_ticks, self.prices[short_ticks], 'ro')
+        plt.plot(long_ticks, self.prices[long_ticks], 'go')
+        plt.plot(do_nothing_ticks, self.prices[do_nothing_ticks], 'yo')
+
+        plt.suptitle(
+            "Total Reward: %.6f" % self._total_reward + ' ~ ' +
+            "Total Profit: %.6f" % self._total_profit
+        )
+        
+        
+    def close(self):
+        plt.close()
+
+
+    def save_rendering(self, filepath):
+        plt.savefig(filepath)
+
+
+    def pause_rendering(self):
+        plt.show()
+
+
+    def _process_data(self):
+        prices = self.df.loc[:, 'Close'].to_numpy()
+
+        prices[self.frame_bound[0] - self.window_size]  # validate index (TODO: Improve validation)
+        prices = prices[self.frame_bound[0]-self.window_size:self.frame_bound[1]]
+
+        diff = np.insert(np.diff(prices), 0, 0)
+        signal_features = np.column_stack((prices, diff))
+
+        return prices, signal_features
+
+
+    def _update_profit(self, action):
+        trade = False
+        if ((action == Actions.Buy.value and self._position == Positions.Short) or
+            (action == Actions.Sell.value and self._position == Positions.Long)):
+            trade = True
+
+        if trade or self._done:
+            current_price = self.prices[self._current_tick]
+            last_trade_price = self.prices[self._last_trade_tick]
+
+            if self._position == Positions.Long:
+                shares = (self._total_profit * (1 - self.trade_fee_ask_percent)) / last_trade_price
+                self._total_profit = (shares * (1 - self.trade_fee_bid_percent)) * current_price
+
+
+    def max_possible_profit(self):
+        current_tick = self._start_tick
+        last_trade_tick = current_tick - 1
+        profit = 1.
+
+        while current_tick <= self._end_tick:
+            position = None
+            if self.prices[current_tick] < self.prices[current_tick - 1]:
+                while (current_tick <= self._end_tick and
+                       self.prices[current_tick] < self.prices[current_tick - 1]):
+                    current_tick += 1
+                position = Positions.Short
+            else:
+                while (current_tick <= self._end_tick and
+                       self.prices[current_tick] >= self.prices[current_tick - 1]):
+                    current_tick += 1
+                position = Positions.Long
+
+            if position == Positions.Long:
+                current_price = self.prices[current_tick - 1]
+                last_trade_price = self.prices[last_trade_tick]
+                shares = profit / last_trade_price
+                profit = shares * current_price
+            last_trade_tick = current_tick - 1
+
+        return profit
